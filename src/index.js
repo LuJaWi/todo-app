d2df810cff5c43a87f8f593a56a57962f91343b7
Luke Witt <Witt.Luke@gmail.com>
2022-06-02 20:50:22 -0400

    add TaskCreator factory function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+// Create factory function for adding tasks
+// Properties for task description, due date, 
+// priority, and completion status
+
+const TaskCreator = () => {
+  let taskDescription = "";
+  let dueDate = 0;
+  // Value of 1-5 (1 is lowest)
+  let taskPriority = 3;
+  let isComplete = false;
+  return {taskDescription, dueDate, taskPriority, isComplete};
+};
