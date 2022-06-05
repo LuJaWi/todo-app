4be3f01571c5cc996ac48245cd895b85c4d03726
Luke Witt <Witt.Luke@gmail.com>
2022-06-05 12:39:42 -0400

    Added dynamic creation of add task element

--- src/index.js
+++ src/index.js
@@ -17,6 +17,75 @@ const Task = (taskDescription, dueDate, taskPriority) => {
   return {taskDescription, dueDate, taskPriority, isComplete};
 };
 
+const addTask = () => {
+  const addTaskElement = document.createElement('div');
+  addTaskElement.classList.add('add-task-prompt');
+
+  const formElement = document.createElement('form');
+  formElement.classList.add('task-form');
+  addTaskElement.appendChild(formElement);
+
+  // Task Descripition Field
+  const taskDescriptionLabel = document.createElement('label');
+  taskDescriptionLabel.htmlFor = 'task-description';
+  taskDescriptionLabel.innerText = 'What Needs Doing?';
+  taskDescriptionLabel.classList.add('add-task-field');
+
+  const taskDescriptionInput = document.createElement('input');
+  taskDescriptionInput.type = 'text';
+  taskDescriptionInput.name = 'task-description';
+  taskDescriptionInput.classList.add('add-task-field');
+
+  // Task Priority Field
+  const taskPriorityLabel = document.createElement('label');
+  taskPriorityLabel.htmlFor = 'task-priority';
+  taskPriorityLabel.innerText = 'How Important Is It?';
+  taskPriorityLabel.classList.add('add-task-field');
+
+  const taskPriorityInput = document.createElement('input');
+  taskPriorityInput.setAttribute('list', 'task-priority');
+
+  const taskPriorityList = document.createElement('datalist');
+  for (let i=1; i<=5; i++) {
+    const taskPriortyOption = document.createElement('option');
+    taskPriortyOption.setAttribute('value', String(i));
+    taskPriorityList.appendChild(taskPriortyOption);
+  };
+  taskPriorityInput.appendChild(taskPriorityList);
+  taskPriorityInput.classList.add('add-task-field');
+
+  // Task Due Date Field
+  const taskDueDateLabel = document.createElement('label');
+  taskDueDateLabel.htmlFor = 'task-due-date';
+  taskDueDateLabel.innerText = 'When Do You Need This Done By?';
+  taskDueDateLabel.classList.add('add-task-field');
+
+  const taskDueDateInput = document.createElement('input');
+  taskDueDateInput.type = 'date';
+  taskDueDateInput.id = 'task-due-date';
+  taskDueDateInput.name = 'due-date'
+  taskDueDateInput.classList.add('add-task-field');
+
+  // Append fields to form
+  formElement.appendChild(taskDescriptionLabel);
+  formElement.appendChild(document.createElement('br'));
+  formElement.appendChild(taskDescriptionInput);
+  formElement.appendChild(document.createElement('br'));
+  formElement.appendChild(taskPriorityLabel);
+  formElement.appendChild(document.createElement('br'));
+  formElement.appendChild(taskPriorityInput);
+  formElement.appendChild(document.createElement('br'));
+  formElement.appendChild(taskDueDateLabel);
+  formElement.appendChild(document.createElement('br'));
+  formElement.appendChild(taskDueDateInput);
+
+  return addTaskElement;
+};
+
+const taskWindowSelector = document.querySelector('.to-do-window');
+
+taskWindowSelector.appendChild(addTask());
+
 // Click on Add task converts the button to a task
 // creation prompt
 
